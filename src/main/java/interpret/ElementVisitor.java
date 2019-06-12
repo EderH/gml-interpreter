68742cea63b633ef169fa72226c1bf86c0f449e3
EderH <HansjoergEder@gmail.com>
2019-06-12 10:27:57 +0200

    #2 Create a Interpreter with Visitor Pattern

---
+++ src/main/java/interpret/ElementVisitor.java
@@ -0,0 +1,18 @@
+package interpret;
+
+import gml.Edge;
+import gml.Graph;
+import gml.Node;
+import gml.Task;
+
+public interface ElementVisitor {
+
+    public void visit(Graph graph);
+
+    public void visit(Task task);
+
+    public void visit(Edge edge);
+
+    public void visit(Node node);
+
+}
---
+++ src/main/java/interpret/Interpreter.java
@@ -0,0 +1,33 @@
+package interpret;
+
+import gml.Edge;
+import gml.Graph;
+import gml.Node;
+import gml.Task;
+
+public class Interpreter implements ElementVisitor{
+
+    public Interpreter() {
+
+    }
+
+    @Override
+    public void visit(Graph graph) {
+        //logic here
+    }
+
+    @Override
+    public void visit(Edge edge) {
+
+    }
+
+    @Override
+    public void visit(Node node) {
+
+    }
+
+    @Override
+    public void visit(Task task) {
+
+    }
+}
